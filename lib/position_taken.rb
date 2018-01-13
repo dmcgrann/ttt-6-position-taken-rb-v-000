33d0187d64ddd2db81138c935bea57f48d04ab27
David McGrann <dpmcgrann@gmail.com>
2018-01-13 19:34:37 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     return true
-  elsif board[index] == " "
+  elsif board[index] == " " || board[index] == ""
     return false
   end
 end
